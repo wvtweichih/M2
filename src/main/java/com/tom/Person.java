a4e23049a61d7884d4906a846378ce5ff8297798
unknown <user@DESKTOP-IT.worldvision.org.tw>
2019-09-05 13:16:18 +0800

    Initial commit

---
+++ src/main/java/com/tom/Person.java
@@ -0,0 +1,10 @@
+package com.tom;
+
+import com.sun.scenario.effect.impl.sw.java.JSWBlend_SRC_OUTPeer;
+
+public class Person {
+    public void hello(){
+        System.out.println("Hello world");
+    }
+
+}
---
+++ src/main/kotlin/com/tom/Hello.kt
@@ -0,0 +1,15 @@
+package com.tom
+
+fun main() {
+//    println("Hello kotlin")
+//    Human().hello()
+    val h = Human()
+    h.hello()
+}
+
+class Human{
+    fun hello() {
+        println("hello kotlin")
+    }
+
+}

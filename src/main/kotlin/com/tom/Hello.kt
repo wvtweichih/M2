4e1c999c3cafcc73989955918de735cb609b9c1d
wvtweichih <weichih_huang@worldvision.org.tw>
2019-09-06 13:59:09 +0800

    Java & Kotlin data type

--- src/main/kotlin/com/tom/Hello.kt
+++ src/main/kotlin/com/tom/Hello.kt
@@ -3,17 +3,22 @@ package com.tom
 fun main() {
 //    println("Hello kotlin")
 //    Human().hello()
-    val h = Human()
-    h.hello()
-    var age  = 19;
+    val h = Human(weight = 66.5f, height = 1.7f)
+    println(h.bmi())
+    /*var age  = 19;
     age = 20;
     var weight = 66.5f;
     var name : String;
-    name = "Hank";
+    name = "Hank";*/
 
 }
 
-class Human{
+class Human(var weight: Float, var height: Float){
+
+    fun bmi() :Float {
+        val bmi = weight / (height*height)
+        return bmi
+    }
     fun hello() {
         println("hello kotlin")
     }
